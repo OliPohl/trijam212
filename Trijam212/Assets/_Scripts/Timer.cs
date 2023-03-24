41ad5af1f9de9757479218773ac966ab5d203c41
OliverPohl <oliver@pohl66.de>
2023-03-24 16:18:22 +0100

    timer

--- Trijam212/Assets/_Scripts/Timer.cs
+++ Trijam212/Assets/_Scripts/Timer.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float _timer;
+    private TMP_Text _text;
+
     void Start()
     {
-        
+        _timer = 0;
+        _text = GetComponent<TMP_Text>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if(gameObject.activeSelf == false)
+        {
+            _timer = 0;
+        }
+
+        _timer += Time.deltaTime;
+
+        var minutes = (int) (_timer / 60);
+        var seconds = (int) (_timer - minutes * 60);
         
+        _text.text = minutes.ToString() + ":" + seconds.ToString();
     }
 }
